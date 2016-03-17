65fbed0f705be7faaea3fd2470b9d41f4b1f41b4
Michael P. Flaga <michael@flaga.net>
2016-03-16 21:47:45 -0400

    re-organized variables to instance in the convert, for future usage of function

--- ReadWriteFlipFlop.py
+++ ReadWriteFlipFlop.py
@@ -3,21 +3,20 @@ from myhdl import Signal, TristateSignal, intbv, toVerilog, toVHDL, always
 from InOutBuffer import InOutBuffer
 from dff import dff
 
-pins = 10
-
-t = TristateSignal(intbv(1)[pins:])
-dir = Signal(bool(0))
-wr, rst = [Signal(bool(0)) for i in range(2)]
-
 def ReadWriteFlipFlop(t, dir, wr, rst):
-    i = Signal(intbv(0)[pins:])
-    q = Signal(intbv(0)[pins:])
+    i = Signal(intbv(0)[len(t):])
+    q = Signal(intbv(0)[len(t):])
     data = InOutBuffer(t, i, q, dir)
     dff_inst = dff(q, i, wr, rst)
 
     return data, dff_inst
 
 def convert():
+
+    pins = 10
+    t = TristateSignal(intbv(1)[pins:])
+    dir = Signal(bool(0))
+    wr, rst = [Signal(bool(0)) for i in range(2)]
 
     toVerilog(ReadWriteFlipFlop, t, dir, wr, rst)
     toVHDL(ReadWriteFlipFlop, t, dir, wr, rst)
